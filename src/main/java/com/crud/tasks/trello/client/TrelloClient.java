bd024912edcffe880f3cbcecaa8009de4f11bf06
dpszczolek <dpszczolek@gmail.com>
2018-04-26 23:44:38 +0200

    JAR and PostreSQL fixed

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -25,17 +25,14 @@ public class TrelloClient {
     @Autowired
     private TrelloConfig trelloConfig;
 
-    private URI getUrl() {
-        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint+ "members/dpszczolek/boards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken)
+    public List<TrelloBoardDto> getTrelloBoards() {
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloConfig.getTrelloApiEndpoint()+ "members/dpszczolek/boards")
+                .queryParam("key", trelloConfig.getTrelloAppKey())
+                .queryParam("token", trelloConfig.getTrelloToken())
                 .queryParam("fields", "name,id")
                 .queryParam("lists", "all").build().encode().toUri();
-        return url;
-    }
-    public List<TrelloBoardDto> getTrelloBoards() {
         try {
-            TrelloBoardDto[] boardsResponse = restTemplate.getForObject(getUrl(), TrelloBoardDto[].class);
+            TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
             return Arrays.asList(Optional.ofNullable(boardsResponse).orElse(new TrelloBoardDto[0]));
         } catch (RestClientException e) {
             LOGGER.error(e.getMessage(), e);
@@ -45,24 +42,15 @@ public class TrelloClient {
 
 
     public CreatedTrelloCard createNewCard (TrelloCardDto trelloCardDto) {
-        URI url1 = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "cards")
-                .queryParam("key", trelloAppKey)
-                .queryParam("token", trelloToken)
+        URI url1 = UriComponentsBuilder.fromHttpUrl(trelloConfig.getTrelloApiEndpoint() + "cards")
+                .queryParam("key", trelloConfig.getTrelloAppKey())
+                .queryParam("token", trelloConfig.getTrelloToken())
                 .queryParam("name", trelloCardDto.getName())
                 .queryParam("desc", trelloCardDto.getDescription())
                 .queryParam("pos", trelloCardDto.getPos())
                 .queryParam("idList", trelloCardDto.getListId()).build().encode().toUri();
                return restTemplate.postForObject(url1, null, CreatedTrelloCard.class);
     }
-
-    @Value("${trello.api.endpoint.prod}")
-    private String trelloApiEndpoint;
-
-    @Value("${trello.app.key}")
-    private String trelloAppKey;
-
-    @Value("${trello.app.token}")
-    private String trelloToken;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TrelloClient.class);
    }
