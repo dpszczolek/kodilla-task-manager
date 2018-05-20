d253e24b3f1591376d3f720013d68fac2e15bb24
dpszczolek <dpszczolek@gmail.com>
2018-05-20 23:35:04 +0200

    24.2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.trello.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,18 @@ public class MailCreatorService {
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
+    @Autowired
+    private AdminConfig adminConfig;
+
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
         context.setVariable("task_url", "http://localhost:8880/crud");
         context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye", "Best regards,");
+        context.setVariable("preview", "Trello Card created");
+        context.setVariable("company_name", adminConfig.getCompanyName());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
--- src/main/java/com/crud/tasks/trello/config/AdminConfig.java
+++ src/main/java/com/crud/tasks/trello/config/AdminConfig.java
@@ -10,4 +10,11 @@ public class AdminConfig {
 
     @Value("${admin.mail}")
     private String adminMail;
+
+    @Value("${admin.name}")
+    private String adminName;
+
+    @Value("${info.company.name}")
+    private String companyName;
+
 }
